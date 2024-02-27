1fead40610a9226ff2cabed39df0be76f50b18b6
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-02-27 20:34:32 -0300

    points section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.scss'
 import Details from './components/Details/Details'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
+import Points from './components/Points/Points'
 import Register from './components/Register/Register'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Register />
       <Details />
+      <Points />
     </>
   )
 }
